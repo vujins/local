0ce234f296b0495ddc331598b3292eca87d6bfb6
Strahinja Vujin <strahinja.vujin11@gmail.com>
2022-03-01 00:38:12 +0100

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BrightnessModule } from './brightness/brightness.module';
 
 @Module({
-  imports: [BrightnessModule],
+  imports: [ConfigModule.forRoot(), BrightnessModule],
   controllers: [AppController],
   providers: [AppService],
 })
