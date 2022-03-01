1d286aef988afd779a824d5b010fbf87e0c2fec1
Strahinja Vujin <strahinja.vujin11@gmail.com>
2022-03-01 01:26:51 +0100

    brightness complete

--- src/brightness/brightness.controller.ts
+++ src/brightness/brightness.controller.ts
@@ -6,9 +6,16 @@ export class BrightnessController {
   constructor(private readonly brightnessService: BrightnessService) {}
 
   @Post()
-  setBrightness(@Body('sunlight') sunlight: number) {
-    console.log({ sunlight });
+  async setBrightness(@Body('sunlight') sunlight: string) {
+    const sunlightValue = Number.parseInt(sunlight);
 
-    return { sunlight };
+    if (!sunlightValue) return { brightness: 0 };
+
+    const brightness = await this.brightnessService.setBrightness(
+      1,
+      sunlightValue,
+    );
+
+    return { brightness };
   }
 }
--- src/brightness/brightness.service.ts
+++ src/brightness/brightness.service.ts
@@ -6,7 +6,8 @@ import { promisify } from 'util';
 export class BrightnessService {
   private readonly execute = promisify(exec);
 
-  async setBrightness(monitor: number, brightness: number) {
+  async setBrightness(monitor: number, sunlight: number): Promise<number> {
+    const brightness = this.getBrightness(sunlight);
     const command = `DMT "DMT:General:ChangeBrightness ${monitor}:${brightness}"`;
     const { stderr, stdout } = await this.execute(command);
 
@@ -16,5 +17,20 @@ export class BrightnessService {
     if (stderr) {
       console.log('stderr: ', stderr);
     }
+
+    return brightness;
+  }
+
+  /** This will convert sunlight value (0-83000 lux) to brightness 0-100 */
+  private getBrightness(sunlight: number) {
+    const sigmoid = this.getSigmoid(sunlight / 60);
+
+    // sigmoid(0) = 0.5
+    // (sigmoid(x/usporenje) - 0.5)*200 - 100 je max
+    return Math.round((sigmoid - 0.5) * 200);
+  }
+
+  private getSigmoid(z: number) {
+    return 1 / (1 + Math.exp(-z));
   }
 }
